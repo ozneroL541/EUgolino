f2b6fb98e1b3123faeb1e6cc056f0fb709243ae3
Lorenzo Radice <117105676+ozneroL541@users.noreply.github.com>
2024-07-16 15:21:41 +0200

    Update eugolino.py

--- classes/eugolino.py
+++ classes/eugolino.py
@@ -92,8 +92,8 @@ class EUgolino(threading.Thread):
 
     It was specifically designed to download papers from the European Union DataBase.
     As the Conte Ugolino in Dante's Inferno, it is a greedy downloader.
-    As Ugolino della Gherardesca ate from the head of his enemy, 
-    EUgolino feed himself with the knowledge of scientific papers.
+    As Ugolino della Gherardesca eats from the head of his enemy, 
+    EUgolino feeds himself with the knowledge of scientific papers.
     """
     file_in: str
     """Input file containing the list of URLs."""
