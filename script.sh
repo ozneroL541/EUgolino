91b3ede3679300e7ac408009b923835993807f91
Lorenzo Radice <117105676+ozneroL541@users.noreply.github.com>
2024-07-16 15:20:05 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -15,15 +15,8 @@ errlog="error.log"
 # Time Logs File
 timelog="time.log"
 
-
-#tree
-
-echo "--------------------"
 # Make destination directory
 mkdir -p $log_dir
 # Run the main.py script
 echo "((time ($main 1>> $log_dir$outlog 2>> $log_dir$errlog)) &>> $log_dir$timelog) &"
 ((time ($main 1>> $log_dir$outlog 2>> $log_dir$errlog)) &>> $log_dir$timelog) &
-# End
-echo "--------------------"
-#tree
