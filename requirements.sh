26a6a83a584c4ce92c6db7a4e574bf21ba086003
Lorenzo Radice <l.21radice@gmail.com>
2024-07-19 12:14:27 +0200

    Requirements

---
+++ requirements.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+#   file name: requirements.sh
+#   author: Lorenzo Radice
+#   license: European Union Public Licence v. 1.2.
+#
+
+sudo apt update
+sudo apt install -y python3 python3-pip python3-requests python3-bs4 
